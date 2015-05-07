b8b408db9faec7d83739149006aedf5d7d7c7880
ggamoodi <ggamoodi@users.noreply.github.com>
2015-05-07 19:23:50 +0900

    route database, show collection list

--- dbviewer/views.py
+++ dbviewer/views.py
@@ -2,9 +2,11 @@ from dbviewer import app
 from flask import render_template
 import pymongo
 
-@app.route('/')
-def index():
+@app.route('/<db>/')
+def index(db):
     connection = pymongo.MongoClient()
-    dbs = connection.database_names()
+    db = connection.get_database(db)
+    collections = db.collection_names()
+    collections.remove("system.indexes")
 
-    return render_template("index.html", dbs=dbs)
+    return render_template("index.html", collections=sorted(collections))
