074c1e854b348a3f17dcd899be8a5191ca91ceec
Jhyun.Park <Jhyun.Park@iMac.local>
2015-05-07 17:34:50 +0900

    Revert "mongodb connection"
    This reverts commit 4a62a4d42be8b26cce1c8799709d26d87fc6138e.

--- dbviewer/views.py
+++ dbviewer/views.py
@@ -1,10 +1,6 @@
 from dbviewer import app
 from flask import render_template
-import pymongo
 
 @app.route('/')
 def index():
-    connection = pymongo.MongoClient()
-    dbs = connection.database_names()
-
-    return render_template("index.html", text=str(dbs))
+    return render_template("index.html")
