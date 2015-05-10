fe1071d868125303e57fb38480957d94c0075a9c
ggamoodi <ggamoodi@users.noreply.github.com>
2015-05-10 22:41:45 +0900

    template inheritance, divide view db/collection/data

--- dbviewer/views.py
+++ dbviewer/views.py
@@ -2,11 +2,23 @@ from dbviewer import app
 from flask import render_template
 import pymongo
 
-@app.route('/<db>/')
-def index(db):
-    connection = pymongo.MongoClient()
-    db = connection.get_database(db)
-    collections = db.collection_names()
-    collections.remove("system.indexes")
+client = pymongo.MongoClient()
 
-    return render_template("index.html", collections=sorted(collections))
+@app.route('/')
+def index():
+    dbs = client.database_names()
+
+    return render_template("dbs.html", dbs=sorted(dbs))
+
+@app.route('/<db>/')
+def get_collections(db):
+    collections = client.get_database(db).collection_names()
+
+    return render_template("collections.html", db=db, collections=sorted(collections))
+
+@app.route('/<db>/<collection>/')
+def get_data(db, collection):
+    collection = client.get_database(db).get_collection(collection)
+    result = collection.find().limit(10)
+
+    return render_template("data.html", result=result)
